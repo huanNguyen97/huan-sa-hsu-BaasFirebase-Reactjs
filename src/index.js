03edfe3a71ddcd099732151876aff8b29059c3ab
nguyencaohuan1997 <nguyencaohuan1997@gmail.com>
2021-12-14 10:22:53 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import from owner
+import { Router } from 'react-router-dom';
+import history from './history/history';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/urlCalling/url.js
@@ -0,0 +1,13 @@
+// Main hosting
+const url = 'https://huan-sa-hsu-restful-golang.herokuapp.com';
+
+// Url exported
+export const read_all_games = url + '/';
+export const create_new_game = url + '/create-game';
+export const read_details_game = url + '/read-details/';
+export const edit_game = url + '/update-game/';
+export const delete_game = url + '/delete-game/';
+export const search_game = url + '/search_game/';
+
+// Temporary to avoid bug
+export const config = 'temp';
